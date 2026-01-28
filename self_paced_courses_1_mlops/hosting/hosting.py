8f6869abfcf48273363e556b00502152c16b6040
Saroj Kumar Singh <saroj270983@gmail.com>
2026-01-28 18:09:20 +0530

    Update hosting.py

--- self_paced_courses_1_mlops/hosting/hosting.py
+++ self_paced_courses_1_mlops/hosting/hosting.py
@@ -6,7 +6,7 @@ api = HfApi(token=os.getenv("HF_TOKEN"))
 #api = HfApi(token=userdata.get("HF_TOKEN"))
 api.upload_folder(
     folder_path="self_paced_courses_1_mlops/deployment",
-    repo_id="sarojk8325/PIMA-Diabetes-Prediction"                                         # enter the Hugging Face username here
+    repo_id="sarojk8325/PIMA-Diabetes-Prediction",                                         # enter the Hugging Face username here
     repo_type="space",
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
